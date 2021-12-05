5b054766e54fd315e483ccf803761734a979befd
jackpan <826647196@qq.com>
2021-12-05 21:39:19 +0800

    Upload file analyze

--- loganalyzes/views.py
+++ loganalyzes/views.py
@@ -18,9 +18,11 @@ from django.http import HttpResponseRedirect
 sc = SparkContext()
 sqlContext = SQLContext(sc)
 spark = SparkSession(sc)
-raw_data_files = glob.glob('/Users/jackpan/JackPanDocuments/temporary/tet/edp.2021-12-02.out')
-base_df = spark.read.text(raw_data_files)
-normal_log_df = base_df.filter(base_df['value'].rlike(r'URI:.*最大内存:.*已分配内存:.*最大可用内存:.*'))
+
+
+# raw_data_files = glob.glob('/Users/jackpan/JackPanDocuments/temporary/tet/edp.2021-12-02.out')
+# base_df = spark.read.text(raw_data_files)
+# normal_log_df = base_df.filter(base_df['value'].rlike(r'URI:.*最大内存:.*已分配内存:.*最大可用内存:.*'))
 
 
 def count_seconds(col_name):
@@ -41,32 +43,17 @@ already_allow_memory_free_pattern = r'(已分配内存中的剩余空间: \d+m)'
 max_useful_memory_free_pattern = r'(最大可用内存: \d+m)'
 count_seconds_udf = udf(lambda z: count_seconds(z), StringType())
 
-performance_log_df = normal_log_df.select(
-    regexp_extract('value', ts_pattern, 1).alias('time'),
-    regexp_extract('value', spend_time_pattern, 1).alias('spend_time'),
-    regexp_extract('value', request_uri_pattern, 1).alias('request_uri'),
-    regexp_extract('value', max_memory_pattern, 1).alias('max_memory'),
-    regexp_extract('value', already_allow_memory_pattern, 1).alias('total_memory'),
-    regexp_extract('value', already_allow_memory_free_pattern, 1).alias('free_memory'),
-    regexp_extract('value', max_useful_memory_free_pattern, 1).alias('max_can_use_memory'),
-).withColumn("spend_time", regexp_replace('spend_time', '耗时：', '')) \
-    .withColumn("spend_time", count_seconds_udf('spend_time').cast("long")) \
-    .withColumn("max_memory", regexp_replace('max_memory', '(最大内存: |m)', '')) \
-    .withColumn("total_memory", regexp_replace('total_memory', '(已分配内存: |m)', '')) \
-    .withColumn("free_memory", regexp_replace('free_memory', '(已分配内存中的剩余空间: |m)', '')) \
-    .withColumn("max_can_use_memory", regexp_replace('max_can_use_memory', '(最大可用内存: |m)', ''))
 
+def index(request):
+    # plot_div = get_hour_pd_df(performance_log_df)
+    # time_div = get_time_pd_df(performance_log_df)
+    # spend_time_div = get_spend_time_div(performance_log_df)
+    # memory_div = get_memory_div(performance_log_df)
 
-def index(request, file_url):
-    print(file_url)
-    plot_div = get_hour_pd_df(performance_log_df)
-    time_div = get_time_pd_df(performance_log_df)
-    spend_time_div = get_spend_time_div(performance_log_df)
-    memory_div = get_memory_div(performance_log_df)
-
-    return render(request, "loganalyzes/index.html", context={'plot_div': plot_div, 'time_div': time_div,
-                                                              'spend_time_div': spend_time_div,
-                                                              'memory_div': memory_div})
+    # return render(request, "loganalyzes/index.html", context={'plot_div': plot_div, 'time_div': time_div,
+    #                                                           'spend_time_div': spend_time_div,
+    #                                                           'memory_div': memory_div})
+    return render(request, "loganalyzes/index.html")
 
 
 def upload_file(request):
@@ -74,26 +61,52 @@ def upload_file(request):
         form = UploadFileForm(request.POST, request.FILES)
         print(form.is_valid())
         if form.is_valid():
-            file_url = handle_uploaded_file(request.FILES['file'])
-            return redirect("index", file_url)
+            file_url = '/Users/jackpan/JackPanDocuments/temporary/tet/edp_temp.out'
+            handle_uploaded_file(request.FILES['file'], file_url)
+            performance_log_df = analyze_edp_log_offline(file_url)
+            plot_div = get_hour_pd_df(performance_log_df)
+            time_div = get_time_pd_df(performance_log_df)
+            spend_time_div = get_spend_time_div(performance_log_df)
+            memory_div = get_memory_div(performance_log_df)
+            return render(request, "loganalyzes/index.html", context={'plot_div': plot_div, 'time_div': time_div,
+                                                                      'spend_time_div': spend_time_div,
+                                                                      'memory_div': memory_div})
     else:
         form = UploadFileForm()
-    return render(request, 'loganalyzes/upload.html', {'form': form})
+        return render(request, 'loganalyzes/upload.html', {'form': form})
 
 
+def analyze_edp_log_offline(file_url):
+    raw_data_files = glob.glob(file_url)
+    base_df = spark.read.text(raw_data_files)
+    normal_log_df = base_df.filter(base_df['value'].rlike(r'URI:.*最大内存:.*已分配内存:.*最大可用内存:.*'))
+    performance_log_df = normal_log_df.select(
+        regexp_extract('value', ts_pattern, 1).alias('time'),
+        regexp_extract('value', spend_time_pattern, 1).alias('spend_time'),
+        regexp_extract('value', request_uri_pattern, 1).alias('request_uri'),
+        regexp_extract('value', max_memory_pattern, 1).alias('max_memory'),
+        regexp_extract('value', already_allow_memory_pattern, 1).alias('total_memory'),
+        regexp_extract('value', already_allow_memory_free_pattern, 1).alias('free_memory'),
+        regexp_extract('value', max_useful_memory_free_pattern, 1).alias('max_can_use_memory'),
+    ).withColumn("spend_time", regexp_replace('spend_time', '耗时：', '')) \
+        .withColumn("spend_time", count_seconds_udf('spend_time').cast("long")) \
+        .withColumn("max_memory", regexp_replace('max_memory', '(最大内存: |m)', '')) \
+        .withColumn("total_memory", regexp_replace('total_memory', '(已分配内存: |m)', '')) \
+        .withColumn("free_memory", regexp_replace('free_memory', '(已分配内存中的剩余空间: |m)', '')) \
+        .withColumn("max_can_use_memory", regexp_replace('max_can_use_memory', '(最大可用内存: |m)', ''))
+
+    return performance_log_df
 
 
-def handle_uploaded_file(f):
-    file_url = '/Users/jackpan/JackPanDocuments/temporary/tet/edp_temp.out'
+def handle_uploaded_file(f, file_url):
     with open(file_url, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
-    return file_url
 
 
 def get_memory_div(data_df):
     # 已分配内存 和 剩余内存变化率
-    already_used_memory_df = performance_log_df.select(col('time'), col('total_memory'), col('free_memory')) \
+    already_used_memory_df = data_df.select(col('time'), col('total_memory'), col('free_memory')) \
         .orderBy(asc('time'))
     already_used_memory_pd_df = (already_used_memory_df.toPandas())
     x_data = []
@@ -152,7 +165,7 @@ def get_hour_pd_df(data_df):
 
 
 def get_time_pd_df(data_df):
-    time_df = performance_log_df.select(col('time')).groupBy(window(col('time'), '1 minutes')).count().orderBy(
+    time_df = data_df.select(col('time')).groupBy(window(col('time'), '1 minutes')).count().orderBy(
         'window')
     time_pd_df = (time_df.toPandas())
     x_data = []
